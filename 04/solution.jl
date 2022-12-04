c5f8a5f01fa9328601c4f76690dfadc5ad9861bb
Taavi Repän <trepan@gmail.com>
2022-12-04 11:58:08 +0200

    part2

--- 04/solution.jl
+++ 04/solution.jl
@@ -2,9 +2,16 @@ function fullycontained(start1, end1, start2, end2)
     return start1 <= start2 <= end2 <= end1
 end
 
-result = 0
+function overlaps(start1, end1, start2, end2)
+    return (start1 <= start2 <= end1) || (start1 <= end2 <= end1)
+end
+
+part1 = 0
+part2 = 0
 for line in eachline(stdin)
     start1,end1,start2,end2 = map(x->parse(Int64, x), match(r"(\d+)-(\d+),(\d+)-(\d+)", line))
-	global result += fullycontained(start1, end1, start2, end2) || fullycontained(start2, end2, start1, end1)
+	global part1 += fullycontained(start1, end1, start2, end2) || fullycontained(start2, end2, start1, end1)
+    global part2 += overlaps(start1, end1, start2, end2) || overlaps(start2, end2, start1, end1)
 end
-@show result
+@show part1
+@show part2
