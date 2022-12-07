02d487bba73bbcb765699d0771d81a4fb513b06f
Taavi Repän <trepan@gmail.com>
2022-12-07 07:35:49 +0200

    part1

---
+++ 07/solution.jl
@@ -0,0 +1,33 @@
+using DataStructures: DefaultDict
+
+fs = DefaultDict(0)
+cur = ["/"]
+
+for line in readlines(stdin)
+    global cur, fs
+    if line == "\$ cd /"
+        cur = ["/"]
+    elseif line == "\$ cd .."
+       pop!(cur) 
+    elseif startswith(line, "\$ cd")
+        name = match(r"\$ cd (.+)", line)[1]
+        push!(cur, name * "/")
+    elseif line == "\$ ls"
+
+    elseif !startswith(line, "dir")
+        m = match(r"(\d+) (.+)", line)
+        p = ""
+        for d in cur
+            p = p * d
+            fs[p] += parse(Int64, m[1])
+        end
+    end
+end
+
+task1 = 0
+for (k,v) in fs
+    if v <= 100000
+        global task1 += v
+    end
+end
+@show task1
