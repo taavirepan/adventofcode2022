419b76c1f2a6fccf49756ce69ed40cd2d786318f
Taavi Repän <trepan@gmail.com>
2022-12-08 12:55:01 +0200

    alternative solution for part 1

---
+++ 08/task1_v2.jl
@@ -0,0 +1,27 @@
+rows = []
+for line in readlines(stdin)
+    global rows, task1
+    push!(rows, Vector{Char}(line) .- '0')
+end
+heights = hcat(rows...)
+
+# max that slowly increases, so that only trees strictly larger then previous trees are visible.
+# with regular max() below we would also count the trees that are equal in height previous highest tree
+function maxincr(a, b)
+  if a < b
+    return b
+  end
+  if a == b
+    return a + 1e-6
+  end
+  return a
+end
+
+direction1 = accumulate(maxincr, heights, dims=1, init=-1)
+direction2 = reverse(accumulate(maxincr, reverse(heights, dims=1), dims=1, init=-1), dims=1)
+direction3 = accumulate(maxincr, heights, dims=2, init=-1)
+direction4 = reverse(accumulate(maxincr, reverse(heights, dims=2), dims=2, init=-1), dims=2)
+
+threshold = min.(direction1, direction2, direction3, direction4)
+
+@show sum(heights .>= threshold)
