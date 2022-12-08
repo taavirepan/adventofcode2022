d47ba2f58f5bc25fddc4be8e1ea391c245ea85d1
Taavi Repän <trepan@gmail.com>
2022-12-08 07:49:27 +0200

    part2

--- 08/solution.jl
+++ 08/solution.jl
@@ -5,42 +5,37 @@ for line in readlines(stdin)
 end
 heights = hcat(rows...)
 visible = zero(heights)
+scenic = zero(heights) .+ 1
 N = size(heights)[1]
 
-for i = 1:N
-    cur = -1
-    for j = 1:N
-        if heights[i,j] > cur
-            visible[i,j] = 1
-            cur = heights[i,j]
-        end
-    end
-    cur = -1
-    for j = N:-1:1
-        if heights[i,j] > cur
-            visible[i,j] = 1
-            cur = heights[i,j]
-        end
-    end
-end
-for j = 1:N
-    cur = -1
-    for i = 1:N
-        if heights[i,j] > cur
-            visible[i,j] = 1
-            cur = heights[i,j]
-        end
-    end
-    cur = -1
-    for i = N:-1:1
-        if heights[i,j] > cur
-            visible[i,j] = 1
-            cur = heights[i,j]
+directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+function walk(i, j, dir)
+    di, dj = dir
+    for d = 1:N
+        if (1 <= i + di*d <= N) && (1 <= j + dj*d <= N)
+            if heights[i+di*d,j+dj*d] >= heights[i,j]
+                return d, false
+            end
+        else
+            return d - 1, true
         end
     end
 end
 
+for i = 1:N
+    for j = 1:N
+        for direction in directions
+            dist, border = walk(i, j, direction)
+            if border
+                visible[i,j] = 1
+            end
+            scenic[i,j] *= dist
+        end
+    end
+end
 
 @show transpose(heights)
 @show transpose(visible)
 @show sum(visible)
+@show maximum(scenic)
