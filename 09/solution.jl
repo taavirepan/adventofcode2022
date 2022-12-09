42f824d62cf9b7dcc99ca29d7478b8d9c11f122f
Taavi Repän <trepan@gmail.com>
2022-12-09 07:41:04 +0200

    some cleanups

--- 09/solution.jl
+++ 09/solution.jl
@@ -1,21 +1,17 @@
 function step(head, tail)
     d = head - tail
-    if d[1] == 0 && abs(d[2]) > 1
+    l = sum(abs.(d))
+    if any(x == 0 for x in d) && l > 1
         return clamp.(d, -1, 1)
-    end
-    if d[2] == 0 && abs(d[1]) > 1
-        return clamp.(d, -1, 1)
-    end
-    if sum(abs.(d)) <= 2
+    elseif l <= 2
         return false
     end
     return clamp.(d, -1, 1)
 end
 
 directions = Dict('R' => [1, 0], 'L' => [-1, 0], 'U' => [0, 1], 'D' => [0, -1])
-rope = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-places = Set()
-push!(places, [0,0])
+rope = fill([0,0], 10)
+places = Dict(1 => Set([[0, 0]]), 9 => Set([[0, 0]]))
 for line in readlines(stdin)
     global rope
     direction = directions[line[1]]
@@ -25,14 +21,13 @@ for line in readlines(stdin)
         for j in 1:9
             while step(rope[j], rope[j+1]) != false
                 rope[j+1] += step(rope[j], rope[j+1])
-                if j == 9
-                    push!(places, rope[10])
+                if j in keys(places)
+                    push!(places[j], rope[j+1])
                 end
             end
         end
     end
 
 end
-@show length(places)
-# 6987 wrong
-# 6168 wrong
+@show length(places[1])
+@show length(places[9])
