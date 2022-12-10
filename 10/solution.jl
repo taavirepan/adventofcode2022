59713eff00ade5f91a742a06e9cebe1ba00582a7
Taavi Repän <trepan@gmail.com>
2022-12-10 09:49:57 +0200

    part 2

--- 10/solution.jl
+++ 10/solution.jl
@@ -1,5 +1,12 @@
 function cycle()
-    global register, current_cycle, task1
+    global register, current_cycle, task1, task2
+    current_x = current_cycle % 40
+    current_y = (current_cycle ÷ 40) % 6
+    if register - 1 <= current_x <= register + 1
+        task2[1 + current_y][1 + current_x] = '#'
+    else
+        task2[1 + current_y][1 + current_x] = '.'
+    end
     current_cycle += 1
     if current_cycle in [20, 60, 100, 140, 180, 220]
         task1 += current_cycle * register
@@ -9,6 +16,7 @@ end
 register = 1
 current_cycle = 0
 task1 = 0
+task2 = [fill('.', 40) for i = 1:6]
 for line in readlines(stdin)
     if line == "noop"
         cycle()
@@ -19,3 +27,4 @@ for line in readlines(stdin)
     end
 end
 @show task1
+println(join([join(line, "") for line in task2], "\n"))
