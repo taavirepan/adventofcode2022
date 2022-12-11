c8299e83126b4d9f41bca27ab6a1e078d212cfd8
Taavi Repän <trepan@gmail.com>
2022-12-11 16:29:14 +0200

    Rust solution for part 2

--- 10/solution.rs
+++ 10/solution.rs
@@ -1,17 +1,25 @@
 use std::io;
 use std::io::Read;
 use std::convert::TryInto;
+use std::convert::TryFrom;
 
 fn main()
 {
     let mut data = String::new();
-    io::stdin().read_to_string(&mut data).expect(":(");
+    io::stdin().read_to_string(&mut data).unwrap();
 	let numbers = data.split_ascii_whitespace().map(|x| x.parse::<i32>());
-    let mut x = 1;
+    let mut x: i32 = 1;
     let mut task1 = 0;
+    let mut task2 = ['.'; 40*6];
     for (n, number) in numbers.enumerate()
     {
         let i:i32 = (n + 1).try_into().unwrap();
+        let sx = n % 40;
+        let sy = n / 40;
+        if i32::try_from(sx).unwrap() <= x + 1 && x <= i32::try_from(sx + 1).unwrap()
+        {
+            task2[sx + sy*40] = '#';
+        }
         if vec![20, 60, 100, 140, 180, 220].contains(&i)
         {
             task1 += i * x;
@@ -23,4 +31,8 @@ fn main()
         };
     }
     println!("task1 = {task1}");
+    for i in 0..6
+    {
+        println!("{}", task2[i*40..(i+1)*40].iter().collect::<String>());
+    }
 }
