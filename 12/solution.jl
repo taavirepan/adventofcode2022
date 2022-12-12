6cdd89f030559179f2d3d603f3893d2e8dd99595
Taavi Repän <trepan@gmail.com>
2022-12-12 07:50:43 +0200

    make it shorter

--- 12/solution.jl
+++ 12/solution.jl
@@ -4,18 +4,10 @@ function search(graph, from, to)
     push!(next, (from, 0))
     graph[from] = 'a'
     graph[to] = 'z'
-    while true
-        sort!(next, lt=(a,b)->a[2] > b[2])
-        if isempty(next)
-            return 9999999
-        end
-
+    while !isempty(next)
         current, distance = pop!(next)
-        while current in visited
-            if isempty(next)
-                return 9999999
-            end
-            current, distance = pop!(next)
+        if current in visited
+            continue
         end
         push!(visited, current)
         if current == to
@@ -24,22 +16,14 @@ function search(graph, from, to)
         for (dx,dy) in [[1, 0], [-1, 0], [0, 1], [0, -1]]
             idx = CartesianIndex(current[1] + dx, current[2] + dy)
             if checkbounds(Bool, graph, idx) && (graph[idx] <= graph[current] + 1) && !(idx in visited)
-                push!(next, (idx, distance + 1))
+                pushfirst!(next, (idx, distance + 1))
             end
         end
     end
-    return distance
+    return 99999
 end
 
-N = 0
-for (i,line) in enumerate(readlines(stdin))
-    global N, data
-    if N == 0
-        N = length(line)
-        data = zeros(Int, N, 0)
-    end
-    data = hcat(data, collect(line))
-end
+data = hcat([collect(line) for line in readlines(stdin)]...)
 
 to = findfirst(data .== 'E')
 @show search(data, findfirst(data .== 'S'), to)
