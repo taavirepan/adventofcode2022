868febd6a0dda5dd7582c3d1b95d57cf58a875c5
Taavi Repän <trepan@gmail.com>
2022-12-13 07:20:53 +0200

    part 2

--- 13/solution.jl
+++ 13/solution.jl
@@ -20,11 +20,33 @@ compare(a::Vector, b::Integer) = compare(a, [b])
 compare(a::Integer, b::Vector) = compare([a], b)
 
 task1 = 0
+alldata = Vector{Any}()
+push!(alldata, [[2]])
+push!(alldata, [[6]])
 for (i, data) in enumerate(Iterators.partition(readlines(stdin), 3))
     a = eval(Meta.parse(data[1]))
     b = eval(Meta.parse(data[2]))
+    push!(alldata, a)
+    push!(alldata, b)
     if compare(a,b)
         global task1 += i
     end
 end
 @show task1
+
+function lt(a,b)
+    r = compare(a,b)
+    if r == nothing
+        return false
+    end
+    return r
+end
+sort!(alldata, lt=lt)
+
+task2 = 1
+for (i,x) in enumerate(alldata)
+    if x == [[2]] || x == [[6]]
+        global task2 *= i
+    end
+end
+@show task2
