5d38f28f5801c377831f7aabcfd431d21daf8209
Taavi Repän <trepan@gmail.com>
2022-12-14 07:20:36 +0200

    part 2

--- 14/solution.jl
+++ 14/solution.jl
@@ -1,4 +1,6 @@
 function draw!(cave, xy1, xy2)
+    xy1 = [xy1[1], xy1[2] + 1]
+    xy2 = [xy2[1], xy2[2] + 1]
     d = clamp.(xy2 .- xy1, -1, 1)
     while xy1 != xy2
         cave[xy1...] = 1
@@ -26,21 +28,24 @@ function fallingsand!(cave, x, y)
 end
 
 cave = zeros(1000, 500)
+floor = 0
 for line in readlines(stdin)
+    global floor
     data = map(x->map(y->parse(Int,y), split(x, ",")), split(line, " -> "))
     for i = 2:length(data)
+        floor = max(floor, data[i-1][2] + 2)
+        floor = max(floor, data[i][2] + 2)
         @assert data[i-1][1] > 0
         @assert data[i][1] > 0
         draw!(cave, data[i-1], data[i])
     end
 end
+cave[:,floor+1] .= 1
 
-for i = 0:10000
-    if fallingsand!(cave, 500, 1)
-        @show i
+for i = 0:100000
+    @assert !fallingsand!(cave, 500, 1)
+    if cave[500, 1] != 0
+        @show i+1
         break
     end
 end
-
-# 311 wrong
-# 780 wrong
