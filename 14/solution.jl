e7694667a4f18e734253e00090f982933d3b72b8
Taavi Repän <trepan@gmail.com>
2022-12-14 07:27:57 +0200

    Small cleanups

--- 14/solution.jl
+++ 14/solution.jl
@@ -1,6 +1,4 @@
 function draw!(cave, xy1, xy2)
-    xy1 = [xy1[1], xy1[2] + 1]
-    xy2 = [xy2[1], xy2[2] + 1]
     d = clamp.(xy2 .- xy1, -1, 1)
     while xy1 != xy2
         cave[xy1...] = 1
@@ -33,19 +31,25 @@ for line in readlines(stdin)
     global floor
     data = map(x->map(y->parse(Int,y), split(x, ",")), split(line, " -> "))
     for i = 2:length(data)
-        floor = max(floor, data[i-1][2] + 2)
-        floor = max(floor, data[i][2] + 2)
-        @assert data[i-1][1] > 0
-        @assert data[i][1] > 0
-        draw!(cave, data[i-1], data[i])
+        floor = max(floor, data[i-1][2])
+        floor = max(floor, data[i][2])
+        draw!(cave, data[i-1] + [0, 1], data[i] + [0, 1])
     end
 end
-cave[:,floor+1] .= 1
+task1 = copy(cave)
+cave[:,floor + 3] .= 1
+
+for i = 0:1000
+    if fallingsand!(task1, 500, 1)
+        println("task1 = ", i)
+        break
+    end
+end
 
 for i = 0:100000
-    @assert !fallingsand!(cave, 500, 1)
+    fallingsand!(cave, 500, 1)
     if cave[500, 1] != 0
-        @show i+1
+        println("task2 = ", i + 1)
         break
     end
 end
