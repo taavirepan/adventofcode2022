186c163bacddcfb7163499ae9cbb17d1863f9114
Taavi Repän <trepan@gmail.com>
2022-12-15 09:40:09 +0200

    Slightly different and faster formulation of the idea. More headache though.

--- 15/solution.jl
+++ 15/solution.jl
@@ -14,37 +14,111 @@ function closest_is_away(sensors, x, y)
     return true
 end
 
-function process!(sensors, beacons, candidates, beacon)
-    sx, sy, bx, by = beacon
-    distance = abs(sx - bx) + abs(sy - by)
-    push!(sensors, (sx, sy, distance))
-    if by == yyy
-        push!(beacons, bx)
+function clip(a, b)
+    v = clamp.(b - a, -1, 1)
+    
+    if a[2] < 0
+        a += v*-a[2]
     end
-    for c in candidates
-        if dist(c..., sx, sy) <= distance
-            setdiff!(candidates, [c])
-        end
+    if a[1] < 0
+        a += v*-a[1]
+    elseif a[1] > 2*yyy
+        a += v*(a[1] - 2*yyy)
     end
-    for y = max(0,sy-distance-1):min(2*yyy,sy+distance+1)
-        x1 = (distance + 1) - abs(y - sy) + sx
-        x2 = -(distance + 1) + abs(y - sy) + sx
-        if 0 <= x1 && x1 <= 2*yyy && closest_is_away(sensors, x1, y)
-            push!(candidates, (x1, y))
-        end
-        if 0 <= x2 && x2 <= 2*yyy && closest_is_away(sensors, x2, y)
-            push!(candidates, (x2, y))
-        end
+    
+    if b[2] > 2*yyy
+        b -= v*(b[2] - 2*yyy)
+    end
+    if b[1] < 0
+        b -= v*-b[1]
+    elseif b[1] > 2*yyy
+        b -= v*(b[1] - 2*yyy)
+    end
+    return a,b
+end
+
+still_out(a) = a[1] < 0 || a[2] < 0 || a[1] > 2*yyy || a[2] > 2*yyy
+
+function edgeintersect(start, stop, sensor, distance)
+    # This could be even faster, if we find the interesections using
+    # math, not while loops. But my head hurts already.
+    direction = clamp.(stop - start, -1, 1)
+    hd = start[1] - sensor[1] + (sensor[2] - start[2]) * direction[1]
+    if hd > distance
+        return [(start, stop)]
+    end
+    while dist(start..., sensor...) < distance
+        start += direction
+    end
+    while dist(stop..., sensor...) < distance
+        stop -= direction
+    end
+    cut1 = start
+    while cut1 != stop && dist(cut1..., sensor...) >= distance
+        cut1 += direction
+    end
+    cut2 = stop
+    while cut2 != start && dist(cut2..., sensor...) >= distance
+        cut2 -= direction
+    end
+    if cut1 == stop
+        return [(start, stop)]
+    else
+        return [(start, cut1), (cut2, stop)]
     end
 end
 
-sensors = []
-beacons = Set()
-candidates = Set()
-for (i,line) in enumerate(readlines(stdin))
-    beacon = map(x->parse(Int,x.match), eachmatch(r"-?\d+", line))
-    process!(sensors, beacons, candidates, beacon)
-    @show i, length(candidates)
+function solved(sensors, a, b)
+    v = clamp.(b - a, -1, 1)
+    a, b = clip(a, b)
+    if a[2] > b[2] || still_out(a) || still_out(b)
+        return false
+    end
+    for (i,(sx,sy,d)) in enumerate(sensors)
+        if dist(a..., sx, sy) <= d && dist(b..., sx, sy) <= d
+            return false
+        end
+        segments = edgeintersect(a, b, [sx, sy], d + 1)
+
+        if length(segments) == 1
+            a, b = segments[1]
+        else
+            for (a,b) in segments
+                if solved(sensors[i+1:end], a, b)
+                    @show a, b, b-a
+                    return true
+                end
+            end
+            return false
+        end
+
+        if a[2] > b[2] || still_out(a) || still_out(b)
+            return false
+        end
+    end
+    @show a, b, b-a
+    return true
 end
-sort!(sensors)
-@show candidates
+
+sensors = []
+for (i,line) in enumerate(readlines(stdin))
+    sx,sy,bx,by = map(x->parse(Int,x.match), eachmatch(r"-?\d+", line))
+    d = abs(sx-bx)+abs(sy-by)
+    push!(sensors, (sx,sy,d))
+end
+sort!(sensors,lt=(a,b)->a[2]-a[3]<b[2]+b[3])
+
+for (sx,sy,d) in sensors
+    if solved(sensors, [sx, sy-d-1], [sx-d-1, sy])
+        break
+    end
+    if solved(sensors, [sx, sy-d-1], [sx+d+1, sy])
+        break
+    end
+    if solved(sensors, [sx-d-1, sy], [sx, sy+d+1])
+        break
+    end
+    if solved(sensors, [sx+d+1, sy], [sx, sy+d+1])
+        break
+    end
+end
