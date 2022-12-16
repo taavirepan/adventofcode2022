7e35188c8435b17904b28e3b66d9e9743ede8747
Taavi Repän <trepan@gmail.com>
2022-12-16 13:50:03 +0200

    fixes to the slow solution

--- 16/solution.jl
+++ 16/solution.jl
@@ -3,47 +3,57 @@ function key(position1, position2, time, opened)
     for o in opened
         ret += 2^(bits[o])
     end
-    return ret, time, position1, position2
-    ret += 2^16 * time
-    ret += 2^24 * index[position1]
-    ret += 2^32 * index[position2]
-    return ret
+    return ret, time, min(position1, position2), max(position1, position2)
 end
 
 function find_best_subsolution(p1, p2, time, opened)
+    if !(p1 isa Array) && !(p2 isa Array)
+        if flow_rates[p1] > 0 && flow_rates[p2] > 0 && p1 != p2 && !(p1 in opened) && !(p2 in opened)
+            opened2 = copy(opened)
+            push!(opened2, p1)
+            push!(opened2, p2)
+            ret = find_solution(p1, p2, time - 1, opened2)
+            return Solution(ret.score + (flow_rates[p1]+flow_rates[p2]) * (time - 1), "$(27-time):$(p1)+$(p2);"*ret.valves)
+        end
+        return Solution(0, "")
+    end
+
     varying = p1 isa Array ? p1 : p2
     fixed = p1 isa Array ? p2 : p1
-    ret = 0
+    ret = Solution(0, "")
     if flow_rates[fixed] == 0 || fixed in opened
-        return 0
+        return ret
     end
     for move in varying
         opened2 = copy(opened)
         push!(opened2, fixed)
         ret = max(ret, 
-            find_solution(min(fixed,move), max(fixed,move), time - 1, opened2)
+            find_solution(fixed, move, time - 1, opened2)
         )
     end
-    return ret + flow_rates[fixed] * (time - 1)
+    return Solution(ret.score + flow_rates[fixed] * (time - 1), "$(27-time):$(fixed);"*ret.valves)
 end
 
-cache = Dict()
+struct Solution
+    score::Int
+    valves::String
+end
+Base.isless(a::Solution,b::Solution) = a.score < b.score
+
 function find_solution(position1, position2, time, opened)
-    if time == 0
-        return 0
+    if time == 1
+        return Solution(0, "")
     end
     k = key(position1, position2, time, opened)
     if k in keys(cache)
         return cache[k]
     end
-    ret = find_best_subsolution(position1, links[position2], time, opened)
-    ret = min(ret, find_best_subsolution(links[position1], position2, time, opened))
-
+    ret = find_best_subsolution(position1, position2, time, opened)
+    ret = max(ret, find_best_subsolution(position1, links[position2], time, opened))
+    ret = max(ret, find_best_subsolution(links[position1], position2, time, opened))
     for link1 in links[position1]
         for link2 in links[position2]
-            linkn = min(link1, link2)
-            linkx = max(link1, link2)
-            cur = find_solution(linkn, linkx, time - 1, copy(opened))
+            cur = find_solution(link1, link2, time - 1, opened)
             ret = max(ret, cur)
         end
     end
@@ -69,4 +79,6 @@ for line in readlines(stdin)
     end
     index[tunnel] = length(index)
 end
-@show find_solution("AA", "AA", 30, Set())
+
+cache = Dict()
+@show find_solution("AA", "AA", 26, Set())
