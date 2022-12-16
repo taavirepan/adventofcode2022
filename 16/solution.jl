b5127eb363cd2cf88e9db0f13cc6438afdf65fd5
Taavi Repän <trepan@gmail.com>
2022-12-16 13:51:00 +0200

    awful messy broken code, that by some luck gives out the correct answer

--- 16/solution.jl
+++ 16/solution.jl
@@ -1,84 +1,172 @@
-function key(position1, position2, time, opened)
-    ret = 0
-    for o in opened
-        ret += 2^(bits[o])
-    end
-    return ret, time, min(position1, position2), max(position1, position2)
+using Profile
+
+struct Vertex
+    player1::Int8
+    player2::Int8
+    time::Int8
+    opened::Int16
 end
 
-function find_best_subsolution(p1, p2, time, opened)
-    if !(p1 isa Array) && !(p2 isa Array)
-        if flow_rates[p1] > 0 && flow_rates[p2] > 0 && p1 != p2 && !(p1 in opened) && !(p2 in opened)
-            opened2 = copy(opened)
-            push!(opened2, p1)
-            push!(opened2, p2)
-            ret = find_solution(p1, p2, time - 1, opened2)
-            return Solution(ret.score + (flow_rates[p1]+flow_rates[p2]) * (time - 1), "$(27-time):$(p1)+$(p2);"*ret.valves)
-        end
-        return Solution(0, "")
-    end
+Base.isless(a::Vertex, b::Vertex) = a.time > b.time
 
-    varying = p1 isa Array ? p1 : p2
-    fixed = p1 isa Array ? p2 : p1
-    ret = Solution(0, "")
-    if flow_rates[fixed] == 0 || fixed in opened
+key(v::Vertex) = (v.player1, v.player2, v.time, v.opened)
+
+function max_score(v::Vertex)
+    ret = 0
+    flows = []
+    for (k,b) in bits
+    # for k = 1:length(bits)
+    #     b = 2^k
+        if v.opened&b == 0
+            # d1 = distance_map[v.player1 => k]
+            # d2 = distance_map[v.player2 => k]
+            # ret -= flow * (v.time - 1 - min(d1, d2))
+            push!(flows, -flow_rates[k])
+        end
+    end
+    sort!(flows)
+    for (i,f) in enumerate(flows)
+        i = max(1, i-1)
+        if v.time - i <= 0
+            return ret
+        end
+        ret += f * (v.time - i)
+    end
+    return ret
+end
+
+
+function edges(v)
+    ret = []
+    if v.time <= 0
         return ret
     end
-    for move in varying
-        opened2 = copy(opened)
-        push!(opened2, fixed)
-        ret = max(ret, 
-            find_solution(fixed, move, time - 1, opened2)
-        )
+    global fully_opened
+    if v.opened == fully_opened
+        return [(0, Vertex(1, 1, 0, fully_opened))]
     end
-    return Solution(ret.score + flow_rates[fixed] * (time - 1), "$(27-time):$(fixed);"*ret.valves)
-end
-
-struct Solution
-    score::Int
-    valves::String
-end
-Base.isless(a::Solution,b::Solution) = a.score < b.score
-
-function find_solution(position1, position2, time, opened)
-    if time == 1
-        return Solution(0, "")
+    global el -= time_ns()
+    if flow_rates[v.player1] > 0 && flow_rates[v.player2] > 0 && 
+        v.opened&bits[v.player1] == 0 && v.opened&bits[v.player2] == 0 && 
+        v.player1 != v.player2
+        push!(ret, (-(flow_rates[v.player1]+flow_rates[v.player2])*(v.time-1), Vertex(v.player1, v.player2, v.time - 1, v.opened|bits[v.player1]|bits[v.player2])))
     end
-    k = key(position1, position2, time, opened)
-    if k in keys(cache)
-        return cache[k]
-    end
-    ret = find_best_subsolution(position1, position2, time, opened)
-    ret = max(ret, find_best_subsolution(position1, links[position2], time, opened))
-    ret = max(ret, find_best_subsolution(links[position1], position2, time, opened))
-    for link1 in links[position1]
-        for link2 in links[position2]
-            cur = find_solution(link1, link2, time - 1, opened)
-            ret = max(ret, cur)
+    for link in links[v.player1]
+        if flow_rates[v.player2] != 0 && v.opened&bits[v.player2] == 0
+            push!(ret, (-flow_rates[v.player2]*(v.time-1), Vertex(link, v.player2, v.time - 1, v.opened|bits[v.player2])))
         end
     end
-    cache[k] = ret
+    for link in links[v.player2]
+        if flow_rates[v.player1] != 0 && v.opened&bits[v.player1] == 0
+            push!(ret, (-flow_rates[v.player1]*(v.time-1), Vertex(link, v.player1, v.time - 1, v.opened|bits[v.player1])))
+        end
+    end
+
+    for link1 in links[v.player1]
+        for link2 in links[v.player2]
+            push!(ret, (0, Vertex(link1, link2, v.time - 1, v.opened)))
+        end
+    end
+    el += time_ns()
+    return ret
+end
+
+function shortest_path(vertex)
+    # max_score(x) = 0
+    queue = [vertex]
+    gScore = Dict(vertex => 0)
+    fScore = Dict(vertex => max_score(vertex))
+    visited = Set()
+    ret = 0
+    skipped = 0
+    while length(queue) > 0
+        current = popfirst!(queue)
+        if key(current) in visited
+            skipped += 1
+            continue
+        end
+        push!(visited, key(current))
+        if current.time == 0
+            # if current.opened == fully_opened
+            #     return gScore[current]
+            # end
+            ret = min(ret, gScore[current])
+            empty!(queue)
+            continue
+        end
+        for (cost, target) in edges(current)
+            target = Vertex(min(target.player1, target.player2), max(target.player1, target.player2), target.time, target.opened)
+            g = gScore[current] + cost
+            if !(target in keys(gScore)) || g < gScore[target]
+                gScore[target] = g
+                fScore[target] = g + max_score(target)
+                # i = searchsorted(queue, target; lt=(a,b)->fScore[a]<fScore[b])
+                i = searchsorted(queue, target; by=(a)->fScore[a])
+                insert!(queue, first(i), target)
+            end
+        end
+    end
+    @show length(visited)
+    @show skipped
+    ret
+end
+
+function full_search(graph)
+    ret = Dict()
+    for (k, edges) in graph
+        ret[k=>k] = 0
+        for e in edges
+            ret[k=>e] = 1
+        end
+    end
+    for k in keys(graph)
+        for i in keys(graph)
+            for j in keys(graph)
+                if (i=>k) in keys(ret) && (k=>j) in keys(ret)
+                    if !((i=>j) in keys(ret)) || ret[i=>j] > ret[i=>k] + ret[k=>j]
+                        ret[i=>j] = ret[i=>k] + ret[k=>j]
+                    end
+                end
+            end
+        end
+    end
     return ret
 end
 
 flow_rates = Dict()
 links = Dict()
 bits = Dict()
+fully_opened = 0
 index = Dict()
 for line in readlines(stdin)
     s1, s2 = split(line, ";")
     m = match(r"Valve (\w\w).*rate=(\d+)", s1)
     tunnel = m[1]
-    flow_rates[tunnel] = parse(Int, m[2])
     tunnels = split(split(replace(s2, r"valves? " => s"::"), "::")[2], ", ")
-    for t in tunnels
-        links[tunnel] = tunnels
+    index[tunnel] = 1+length(index)
+    flow_rates[index[tunnel]] = parse(Int, m[2])
+    links[index[tunnel]] = tunnels
+    if flow_rates[index[tunnel]] > 0
+        global fully_opened |= bits[index[tunnel]] = 2^length(bits)
     end
-    if flow_rates[tunnel] > 0
-        bits[tunnel] = length(bits)
-    end
-    index[tunnel] = length(index)
 end
 
-cache = Dict()
-@show find_solution("AA", "AA", 26, Set())
+for i in keys(links)
+    links[i] = [index[t] for t in links[i]]
+end
+
+# distance_map = full_search(links)
+
+# flow_rates = [flow_rates[i] for i=1:length(flow_rates)]
+
+t = -time_ns()
+el = 0
+@show shortest_path(
+    Vertex(index["AA"], index["AA"], 26, 0)
+)
+t += time_ns()
+
+# Profile.print(mincount=1000)
+
+@show 100*el/t
+@show el*1e-9, t*1e-9
