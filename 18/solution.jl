921a0499ff5aedf95d83d83e6ba606f61a881f42
Taavi Repän <trepan@gmail.com>
2022-12-18 09:49:05 +0200

    part2

--- 18/solution.jl
+++ 18/solution.jl
@@ -1,4 +1,4 @@
-function wrong_solution(grid)
+function solve(grid)
     ret = 0
     for x = 1:size(grid,1)-1
         for y = 1:size(grid,2)-1
@@ -18,38 +18,42 @@ function wrong_solution(grid)
     return ret
 end
 
-function slow_solution(cubes)
-    cubes = Set(cubes)
-    ret = 0
-    for (x,y,z) in cubes
-        if !([x-1,y,z] in cubes)
-            ret += 1
-        end
-        if !([x,y-1,z] in cubes)
-            ret += 1
-        end
-        if !([x,y,z-1] in cubes)
-            ret += 1
-        end
-        if !([x+1,y,z] in cubes)
-            ret += 1
-        end
-        if !([x,y+1,z] in cubes)
-            ret += 1
-        end
-        if !([x,y,z+1] in cubes)
-            ret += 1
+function fill_step(grid, fill)
+    ret = false
+    for x = 2:size(grid,1)-1
+        for y = 2:size(grid,2)-1
+            for z = 2:size(grid,3)-1
+                nboring = (fill[x-1,y,z] || fill[x,y-1,z] || fill[x,y,z-1])
+                nboring = nboring || (fill[x+1,y,z] || fill[x,y+1,z] || fill[x,y,z+1])
+                if !grid[x,y,z] && nboring && !fill[x,y,z]
+                    fill[x,y,z] = true
+                    ret = true
+                end
+            end
         end
     end
     return ret
 end
 
-cubes = []
+function floodfill(grid)
+    ret = zero(grid)
+    ret[1,:,:] .= true
+    ret[:,1,:] .= true
+    ret[:,:,1] .= true
+    ret[size(grid,1),:,:] .= true
+    ret[:,size(grid,2),:] .= true
+    ret[:,:,size(grid,3)] .= true
+    while fill_step(grid, ret)
+    end
+    return ret
+end
+
 grid = zeros(Bool, 25, 25, 25)
 for line in readlines(stdin)
     x,y,z = map(x->parse(Int,x.match), eachmatch(r"\d+", line))
-    grid[x+1,y+1,z+1] = true
-    push!(cubes, [x,y,z])
+    grid[x+2,y+2,z+2] = true
 end
-@show wrong_solution(grid)
-@show slow_solution(cubes)
+outside = floodfill(grid)
+@show solve(grid)
+@show solve(grid .| .!outside)
+
