a9d496a184c188606f7cdf707c409e123d0265ee
Taavi Repän <trepan@gmail.com>
2022-12-20 07:40:50 +0200

    part 2

--- 20/solution.jl
+++ 20/solution.jl
@@ -1,18 +1,19 @@
+key = (811589153)
+# key = 1
 data = []
 for (i,line) in enumerate(readlines(stdin))
-    push!(data,parse(Int, line))
+    push!(data,parse(Int, line) * key)
 end
-done = zeros(Bool, length(data))
-for _=1:length(data)
-    i = 1
-    while done[i]
-        i = (i) % length(data) +1
+
+ordering = collect(1:length(data))
+for loops = 1:10
+    for i=1:length(data)
+        i = findfirst(ordering .== i)
+        x = popat!(data, i)
+        j = (i + length(data)*key*3 + x - 1) % length(data) + 1
+        insert!(data, j, x)
+        insert!(ordering, j, popat!(ordering, i))
     end
-    popat!(done, i)
-    x = popat!(data, i)
-    j = (i + length(data)*2 + x - 1) % length(data) + 1
-    insert!(data, j, x)
-    insert!(done, j, true)
 end
 circshift!(data, findfirst(data.==0)-1)
 
